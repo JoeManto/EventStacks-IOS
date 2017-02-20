97fac8f4ecfb27361a674580073e0ada1ac1e47a
Joe Manto <joemanto@surdir.local>
2017-02-19 20:12:36 -0500

    added data object class for events

---
+++ EventStacks/EventData.h
@@ -0,0 +1,13 @@
+//
+//  EventData.h
+//  EventStacks
+//
+//  Created by Joe Manto on 2/19/17.
+//  Copyright © 2017 Joe Manto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventData : NSObject
+
+@end
---
+++ EventStacks/EventData.m
@@ -0,0 +1,13 @@
+//
+//  EventData.m
+//  EventStacks
+//
+//  Created by Joe Manto on 2/19/17.
+//  Copyright © 2017 Joe Manto. All rights reserved.
+//
+
+#import "EventData.h"
+
+@implementation EventData
+
+@end
